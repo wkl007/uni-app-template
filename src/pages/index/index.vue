ef2f886f835bd5a5107aa5dd07c3c725738f12db
wkl--007 <499657357@qq.com>
2020-02-18 17:27:11 +0800

    feat：uni-app项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <img class="logo" src="/static/logo.png" alt="">
+    <view>
+      <text class="title">{{title}}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad () {},
+  methods: {
+    handleClick () {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@function rpx($value) {
+  @return $value*1rpx;
+}
+
+.content {
+  text-align: center;
+  height: rpx(400);
+}
+
+.logo {
+  height: rpx(200);
+  width: rpx(200);
+  margin-top: rpx(200);
+}
+
+.title {
+  font-size: rpx(36);
+  color: #8f8f94;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const TerserPlugin = require('terser-webpack-plugin')// 去console插件
+const CompressionWebpackPlugin = require('compression-webpack-plugin')// gzip压缩插件
+
+const resolve = dir => require('path').join(__dirname, dir)
+console.log(process.env.npm_lifecycle_event)
+
+module.exports = {
+  // 生产环境sourceMap
+  productionSourceMap: false,
+  // webpack配置
+  configureWebpack: config => {
+    // config.name = name
+    const plugins = [
+      // 去console
+      new TerserPlugin({
+        terserOptions: {
+          warnings: false,
+          compress: {
+            drop_debugger: true,
+            drop_console: true,
+          },
+        },
+        sourceMap: false,
+        parallel: true,
+      }),
+    ]
+    // h5打包gzip压缩处理
+    if (process.env.npm_lifecycle_event.includes('h5')) {
+      plugins.push(new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp(
+          '\\.(' +
+          ['js', 'css'].join('|') +
+          ')$',
+        ),
+        threshold: 10240,
+        minRatio: 0.8,
+      }))
+    }
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins = [...config.plugins, ...plugins]
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
+  // webpack-dev-server配置
+  devServer: {
+    open: false, // 打开浏览器
+    overlay: {
+      warnings: false,
+      errors: false
+    },
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hotOnly: false,
+    proxy: null, // 设置代理
+    before: app => {}
+  },
+}
